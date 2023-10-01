ccc493dd97063cbfa7bc690b43a38712a340d227
Asqar A <arslonovasqar123@gmail.com>
2023-10-01 10:52:44 -0700

    feat: change photos

---
+++ src/data/user.js
@@ -0,0 +1,74 @@
+const INFO = {
+	main: {
+		title: "Asqar Arslonov",
+		name: "Asqar Arslonov",
+		email: "asqararslonov2008@gmail.com",
+		logo: "../logo.png",
+	},
+
+	socials: {
+		telegram: "https://telegram.org/asqararslonov",
+		github: "https://github.com/asqararslonov2008",
+		linkedin: "https://www.linkedin.com/in/asqararslonov/",
+		stackoverflow: "https://stackoverflow.com/",
+		facebook: "https://facebook.com/",
+	},
+
+	homepage: {
+		title: "Full-stack web Developer (MERN) & Volunteer & Founder of leksika.uz",
+		description:
+			"I'm Full Stack web developer who mostly focused on Frontend development. I'm also founder of leksika.uz and volunteer. I have a 3 years in experience with Web Development and have strong knowledge in JavaScript technology",
+	},
+
+	about: {
+		title: "I’m Asqar A. I'm uzbek developer and love to contribute to open-source projects",
+		description:
+			"I've worked on a variety of projects over the years and I'm proud of the progress I've made. Many of these projects are open-source and available for others to explore and contribute to. If you're interested in any of the projects I've worked on, please feel free to check out the code and suggest any improvements or enhancements you might have in mind. Collaborating with others is a great way to learn and grow, and I'm always open to new ideas and feedback.",
+	},
+
+	articles: {
+		title: "I'm passionate about pushing the boundaries of what's possible and inspiring the next generation of innovators.",
+		description:
+			"Chronological collection of my long-form thoughts on programming, leadership, product design, and more.",
+	},
+
+	projects: [
+		{
+			title: "Leksika.uz",
+			description:
+				"Leksika.uz is a dictionary for english learners to learn new words in their native language uzbek and in the platform you can discover articles which might help your english learning process",
+			logo: "https://raw.githubusercontent.com/tomchen/stack-icons/master/logos/react.svg	",
+			linkText: "View Project",
+			link: "https://leksika.uz",
+		},
+
+		{
+			title: "Bleather bot",
+			description:
+				"Bleather bot is a telegram bot which provides weather information in uzbekistan region and all the data based on real-time",
+			logo: "https://static-00.iconduck.com/assets.00/node-js-icon-1901x2048-mk1e13df.png",
+			linkText: "View Project",
+			link: "https://t.me/bleather_bot",
+		},
+
+		{
+			title: "Landing page",
+			description:
+				"Just Landing page for builder corporation",
+			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/html/html.png",
+			linkText: "View Project",
+			link: "https://builder-page.vercel.app/",
+		},
+
+		{
+			title: "MiniGame",
+			description:
+				"in this MiniGame you;ve to click on elements in less time, you win if you click more in the limited time",
+			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/javascript/javascript.png",
+			linkText: "View Project",
+			link: "http://mini-game-two.vercel.app/",
+		},
+	],
+};
+
+export default INFO;
